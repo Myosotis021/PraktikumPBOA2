8acaae42aae4e7a4a1491b9ebfc63bbe139c11f3
myosotis021 <ridhotrisaputra0@gmail.com>
2026-02-23 14:46:27 +0700

    pertemuan 2

---
+++ Pertemuan2/src/Titik.java
@@ -0,0 +1,42 @@
+/* Nama File    : Titik.java
+ * Deskripsi    : berisi atribut dan method dalam class Titik
+ * Pembuat      : Ridho Tri Saputra(24060124130122)
+ * Tanggal      : 23/02/2026 */
+
+public class Titik {
+    /*****************ATRIBUT*****************/
+    double absis;
+    double ordinat;
+
+    /*****************METHOD*****************/
+    //konstruktor untuk membuat titik (0,0)
+    public Titik() {
+        absis = 0;
+        ordinat = 0;
+    }
+
+    //mengembalikan nilai absis
+    public void setAbsis(double a) {
+        absis = a;
+    }
+
+    //mengembalikan nilai ordinat
+    public void setOrdinat(double o) {
+        ordinat = o;
+    }
+
+    //mengeset absis titik dengan nilai baru x
+    public double getAbsis() {
+        return absis;
+    }
+
+    //mengeset ordinat titik dengan nilai baru y
+    public double getOrdinat() {
+        return ordinat;
+    }
+
+    //menggeser nilai absis dan ordinat titik masing-masing sejauh x dan y
+    public void printTitik() {
+        System.out.println("(" + absis + "," + ordinat + ")");
+    }
+} //end class Titik
